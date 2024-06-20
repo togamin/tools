f921f54118d0dce65df3a065bc1ca23f7ccfa924
togamin <togaminnnn@gmail.com>
2024-06-21 08:18:34 +0900

    スプレッドシートへの書き込み処理追加

--- app/pomodoro_timer/pomodoro_timer.py
+++ app/pomodoro_timer/pomodoro_timer.py
@@ -1,5 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
 print("pomodoro_timer")
 
 #スプレッドシートへの認証情報の設定
@@ -19,5 +20,10 @@ def get_past_logs():
     last_10_rows = worksheet.get_all_values()[start_row-1:last_row]
     return last_10_rows
 
+#スプレッドシートにログを書き込む
+def log_activity(status, group, activity):
+    worksheet.append_row([datetime.now().strftime("%m/%d"),datetime.now().strftime("%H:%M"), status, group ,activity])
+
 if __name__ == "__main__":
     print(get_past_logs())
+    log_activity("Done", "作業効率", "GitのIssueテンプレートの作成")
