b31bbaf1b600e6851a921c3d4fb5e9d7e95901f2
togamin <togaminnnn@gmail.com>
2024-06-22 14:17:13 +0900

    仮想環境構築/スプシアクセス用ライブラリインストール

--- app/pomodoro_cycle/pomodoro_cycle.py
+++ app/pomodoro_cycle/pomodoro_cycle.py
@@ -1,1 +1,22 @@
-print("pomodoro_timer")
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#スプレッドシートへの認証情報の設定
+CREDENTIALS_FILE = 'config/credentials.json'
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
+client = gspread.authorize(credentials)
+
+# スプレッドシートを開きワークシートを選択する
+spreadsheet = client.open("目標管理シート_togamin")
+worksheet = spreadsheet.worksheet("pomodoro")
+
+#スプレッドシートから過去ログを読み込む
+def get_past_logs():
+    last_row = len(worksheet.col_values(1))
+    start_row = max(last_row - 9, 1)
+    last_10_rows = worksheet.get_all_values()[start_row-1:last_row]
+    return last_10_rows
+
+if __name__ == "__main__":
+    print(get_past_logs())
